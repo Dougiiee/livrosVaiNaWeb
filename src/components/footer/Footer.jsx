31158e107cf388247df887f338f6716bc0d76c65
Dougiiee <dodolaureano@gmail.com>
2024-11-25 10:41:45 -0300

    Adicionando segunda sessao do inicio, alguns erros no codigo e a footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import logoFacebook from '../../assets/logoFacebook.png'
+import logoTwitter from '../../assets/logoTwitter.png'
+import logoYoutube from '../../assets/logoYoutube.png'
+import logoLinkedin from '../../assets/logoLinkedin.png'
+import logoInstagram from '../../assets/logoInstagram.png'
+import s from './footer.module.scss'
+
+export default function Footer() {
+    return(
+    <footer>
+        <section className={s.contatosFooter}>
+            <p>4002-8922</p>
+            <nav>
+                <a href=""><img src={logoFacebook} alt="" /></a>
+                <a href=""><img src={logoTwitter} alt="" /></a>
+                <a href=""><img src="" alt="" /></a>
+                <a href=""><img src="" alt="" /></a>
+                <a href=""><img src="" alt="" /></a>
+            </nav>
+        </section>
+        <section className={s.copyright}>
+            <p>Layout desenvolvido pela Vai na Web para fins educativos - 2024</p>
+
+        </section>
+    </footer>
+        )
+}
--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,4 +1,8 @@
 import s from './Inicio.module.scss'
+import imagem1 from '../../assets/imagem1doacao.png'
+import imagem2 from '../../assets/imagem2doacao.png'
+import imagem3 from '../../assets/imagem3doacao.png'
+import imagem4 from '../../assets/imagem4doacao.png'
 
 export default function Inicio(){
     return(
@@ -7,6 +11,28 @@ export default function Inicio(){
                 <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
                 
             </section>
+            <section className={s.cardsMotivos} >
+                <h2>Por que devo doar?</h2>
+            
+                <section >
+                    <img src={imagem1} alt="imagem ilustrando um circulo social de 4 pessoas" />
+                    <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+                </section>
+                <section>
+                    <img src={imagem2} alt="imagem de uma pessoa lendo" />
+                    <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+                </section>
+                <section>
+                    <img src={imagem3} alt="imagem ilustrando 6 pessoas transformando vidas" />
+                    <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+                </section>
+                    <section>
+                        <img src={imagem4} alt="imagem ilustrando uma balança vazia" />
+                        <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+                    </section>
+                </section>
+                
+            
         </main>
     )
 }
